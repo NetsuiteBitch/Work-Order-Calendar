69bcacdcbc4b8c58cc7429cd142b03d24aa5ce4f
Your Name <you@example.com>
2021-04-28 17:06:00 -0400

    Initial Commit; can't get filter to work

---
+++ src/FileCabinet/SuiteScripts/Add Rows.js
@@ -0,0 +1,217 @@
+/**
+ * @NApiVersion 2.x
+ * @NScriptType ClientScript
+ * @NModuleScope SameAccount
+ */
+define(['N/search'],
+
+function(search) {
+
+    /**
+     * Function to be executed after page is initialized.
+     *
+     * @param {Object} scriptContext
+     * @param {Record} scriptContext.currentRecord - Current form record
+     * @param {string} scriptContext.mode - The mode in which the record is being accessed (create, copy, or edit)
+     *
+     * @since 2015.2
+     */
+    function pageInit(scriptContext) {
+        var record = scriptContext.currentRecord
+        console.log("yolo")
+        var mysearch = search.load({id:'customsearchwork_order_calendar'})
+        var startdate = record.getValue('custpage_current_start')
+        var endate = record.getValue('custpage_current_end')
+        console.log(startdate)
+        console.log(endate)
+
+
+        var newfilters = [];
+
+        newfilters.push(search.createFilter({
+            name: 'startdate',
+            operator: search.Operator.WITHIN,
+            values: ["03/28/2021", "04/28/2021"]
+        }));
+
+
+        var standardsearch = search.load({id: 'customsearch_mfgmob_buildworkorders'});
+        var oldfilters = standardsearch.filters;
+        for (var i = 0; i < newfilters.length; i++) {
+            oldfilters.push(newfilters[i]);
+        }
+        standardsearch.filters = oldfilters;
+        console.log(standardsearch.filters)
+
+        standardsearch.run().each(function (line){
+            console.log("foo")})
+            return true
+        }
+
+
+
+
+        // var parsedresults = getAllSSResults(standardsearch)
+
+    //     function getAllSSResults(s) {
+    //         var results = s.run();
+    //         var searchResults = [];
+    //         var searchid = 0;
+    //         do {
+    //             var resultslice = results.getRange({start:searchid,end:searchid+1000});
+    //             resultslice.forEach(function(slice) {
+    //                     searchResults.push(slice);
+    //                     searchid++;
+    //                 }
+    //             );
+    //         } while (resultslice.length >=1000);
+    //         return searchResults;
+    //     }
+    // }
+
+    /**
+     * Function to be executed when field is changed.
+     *
+     * @param {Object} scriptContext
+     * @param {Record} scriptContext.currentRecord - Current form record
+     * @param {string} scriptContext.sublistId - Sublist name
+     * @param {string} scriptContext.fieldId - Field name
+     * @param {number} scriptContext.lineNum - Line number. Will be undefined if not a sublist or matrix field
+     * @param {number} scriptContext.columnNum - Line number. Will be undefined if not a matrix field
+     *
+     * @since 2015.2
+     */
+    function fieldChanged(scriptContext) {
+
+    }
+
+    /**
+     * Function to be executed when field is slaved.
+     *
+     * @param {Object} scriptContext
+     * @param {Record} scriptContext.currentRecord - Current form record
+     * @param {string} scriptContext.sublistId - Sublist name
+     * @param {string} scriptContext.fieldId - Field name
+     *
+     * @since 2015.2
+     */
+    function postSourcing(scriptContext) {
+
+    }
+
+    /**
+     * Function to be executed after sublist is inserted, removed, or edited.
+     *
+     * @param {Object} scriptContext
+     * @param {Record} scriptContext.currentRecord - Current form record
+     * @param {string} scriptContext.sublistId - Sublist name
+     *
+     * @since 2015.2
+     */
+    function sublistChanged(scriptContext) {
+
+    }
+
+    /**
+     * Function to be executed after line is selected.
+     *
+     * @param {Object} scriptContext
+     * @param {Record} scriptContext.currentRecord - Current form record
+     * @param {string} scriptContext.sublistId - Sublist name
+     *
+     * @since 2015.2
+     */
+    function lineInit(scriptContext) {
+
+    }
+
+    /**
+     * Validation function to be executed when field is changed.
+     *
+     * @param {Object} scriptContext
+     * @param {Record} scriptContext.currentRecord - Current form record
+     * @param {string} scriptContext.sublistId - Sublist name
+     * @param {string} scriptContext.fieldId - Field name
+     * @param {number} scriptContext.lineNum - Line number. Will be undefined if not a sublist or matrix field
+     * @param {number} scriptContext.columnNum - Line number. Will be undefined if not a matrix field
+     *
+     * @returns {boolean} Return true if field is valid
+     *
+     * @since 2015.2
+     */
+    function validateField(scriptContext) {
+
+    }
+
+    /**
+     * Validation function to be executed when sublist line is committed.
+     *
+     * @param {Object} scriptContext
+     * @param {Record} scriptContext.currentRecord - Current form record
+     * @param {string} scriptContext.sublistId - Sublist name
+     *
+     * @returns {boolean} Return true if sublist line is valid
+     *
+     * @since 2015.2
+     */
+    function validateLine(scriptContext) {
+
+    }
+
+    /**
+     * Validation function to be executed when sublist line is inserted.
+     *
+     * @param {Object} scriptContext
+     * @param {Record} scriptContext.currentRecord - Current form record
+     * @param {string} scriptContext.sublistId - Sublist name
+     *
+     * @returns {boolean} Return true if sublist line is valid
+     *
+     * @since 2015.2
+     */
+    function validateInsert(scriptContext) {
+
+    }
+
+    /**
+     * Validation function to be executed when record is deleted.
+     *
+     * @param {Object} scriptContext
+     * @param {Record} scriptContext.currentRecord - Current form record
+     * @param {string} scriptContext.sublistId - Sublist name
+     *
+     * @returns {boolean} Return true if sublist line is valid
+     *
+     * @since 2015.2
+     */
+    function validateDelete(scriptContext) {
+
+    }
+
+    /**
+     * Validation function to be executed when record is saved.
+     *
+     * @param {Object} scriptContext
+     * @param {Record} scriptContext.currentRecord - Current form record
+     * @returns {boolean} Return true if record is valid
+     *
+     * @since 2015.2
+     */
+    function saveRecord(scriptContext) {
+
+    }
+
+    return {
+        pageInit: pageInit,
+        // fieldChanged: fieldChanged,
+        // postSourcing: postSourcing,
+        // sublistChanged: sublistChanged,
+        // lineInit: lineInit,
+        // validateField: validateField,
+        // validateLine: validateLine,
+        // validateInsert: validateInsert,
+        // validateDelete: validateDelete,
+        // saveRecord: saveRecord
+    };
+    
+});
---
+++ src/FileCabinet/SuiteScripts/Work Order Calender.js
@@ -0,0 +1,152 @@
+/**
+ * @NApiVersion 2.1
+ * @NScriptType Suitelet
+ */
+define(['N/file', 'N/log', 'N/record', 'N/ui/serverWidget'],
+    /**
+ * @param{file} file
+ * @param{log} log
+ * @param{record} record
+ * @param{serverWidget} serverWidget
+ */
+    (file, log, record, serverWidget) => {
+        /**
+         * Defines the Suitelet script trigger point.
+         * @param {Object} scriptContext
+         * @param {ServerRequest} scriptContext.request - Incoming request
+         * @param {ServerResponse} scriptContext.response - Suitelet response
+         * @since 2015.2
+         */
+        const onRequest = (scriptContext) => {
+
+            var response = scriptContext.response
+            var request = scriptContext.request
+            var form = serverWidget.createForm({
+                title: 'Work Order Calendar'
+            });
+
+
+            var today;
+            var numrange;
+            var daterange;
+            var dateheaderrange;
+            var dateheaders;
+            var startdate;
+            var enddate;
+
+            if (request.method == 'GET'){
+
+             today = new Date(2021,   3, 23);
+             numrange = Array.from(Array(7).keys());
+             daterange = numrange.map(x => formatmmmdd(addDays(today, x)));
+             dateheaderrange = daterange.map(x => "<th>" + x + "</th>")
+             dateheaders = dateheaderrange.join('')
+                startdate = new Date(2021, 3, 23)
+                enddate = addDays(startdate,7)
+            }else{
+                startdate = request.parameters.custpage_start_date !== "" ? new Date(request.parameters.custpage_start_date) : new Date()
+                enddate = request.parameters.custpage_end_date !== "" ? new Date(request.parameters.custpage_end_date) : addDays(startdate,7)
+
+                var days = ((enddate - startdate)/(1000 * 60 * 60 * 24)) + 1
+
+                numrange = Array.from(Array(days).keys());
+                daterange = numrange.map(x => formatmmmdd(addDays(startdate, x)));
+                dateheaderrange = daterange.map(x => "<th>" + x + "</th>")
+                dateheaders = dateheaderrange.join('')
+            }
+
+            form.addFieldGroup({
+                id:'custpage_datefilterfield',
+                label: 'Date Filter'
+            })
+
+            form.addFieldGroup({
+                id:'custpage_table',
+                label: 'Schedule'
+            })
+
+            var currentstart = form.addField({
+                id:'custpage_current_start',
+                label: 'Current Start',
+                type: serverWidget.FieldType.DATE,
+                container: 'custpage_datefilterfield'
+            })
+
+            currentstart.defaultValue = startdate
+            currentstart.updateDisplayType({displayType: serverWidget.FieldDisplayType.INLINE})
+
+            var currentend = form.addField({
+                id:'custpage_current_end',
+                label: 'Current End',
+                type: serverWidget.FieldType.DATE,
+                container: 'custpage_datefilterfield'
+            })
+
+            currentend.defaultValue = enddate
+            currentend.updateDisplayType({displayType: serverWidget.FieldDisplayType.INLINE})
+
+
+            form.addSubmitButton({
+                label: 'Filter',
+            });
+
+            var startdatefield = form.addField({
+                id: 'custpage_start_date',
+                type: serverWidget.FieldType.DATE,
+                label: 'Start Date',
+                container: 'custpage_datefilterfield'
+            });
+
+            var enddatefield = form.addField({
+                id: 'custpage_end_date',
+                type: serverWidget.FieldType.DATE,
+                label: 'End Date',
+                container: 'custpage_datefilterfield'
+            });
+
+            var theInlineHTMLFld = form.addField({
+                id: 'custpage_inline_html_fld',
+                type: serverWidget.FieldType.INLINEHTML,
+                label: 'Field for Data Table',
+                container: 'custpage_table'
+            });
+
+            var dtCssCdn = '<link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.20/css/jquery.dataTables.css">';
+            var dtJqueryCdn = '<script type="text/javascript" language="javascript" src="https://code.jquery.com/jquery-3.3.1.js"></script>';
+            var dtJsCdn = '<script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.10.20/js/jquery.dataTables.js"></script>';
+            var dataTableHtml = '<table id="data_table_items" class="display" style="width: 100%"><thead><tr><th>Item</th>' + dateheaders + '</tr></thead><tbody id="data_table_items_body"></tbody></table>';
+            var executeDataTableHtml = '<script>jQuery(document).ready(function (){jQuery("#data_table_items").DataTable();});</script>';
+
+            theInlineHTMLFld.defaultValue = '<!DOCTYPE html><html lang="e"><html><head><meta charset="UTF-8">' + dtCssCdn + dtJqueryCdn + dtJsCdn + '</head><body>' + dataTableHtml + executeDataTableHtml;
+            response.writePage(form)
+
+            form.clientScriptFileId  = "9843"
+
+            function formatmmmdd(dt){
+                const months = ["JAN", "FEB", "MAR","APR", "MAY", "JUN", "JUL", "AUG", "SEP", "OCT", "NOV", "DEC"];
+                return months[dt.getMonth()] + "-" + dt.getDate()
+            }
+
+            function addDays(date, days) {
+                var result = new Date(date);
+                result.setDate(result.getDate() + days);
+                return result;
+            }
+        }
+
+        function days_between(date1, date2) {
+
+            // The number of milliseconds in one day
+            const ONE_DAY = 1000 * 60 * 60 * 24;
+
+            // Calculate the difference in milliseconds
+            const differenceMs = Math.abs(date1 - date2);
+
+            // Convert back to days and return
+            return Math.round(differenceMs / ONE_DAY);
+
+        }
+
+        return {onRequest}
+
+    });
